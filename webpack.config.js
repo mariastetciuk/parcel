14cc0f4c884712d4627f9005bb8c6e9f35e8f50c
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-03-07 23:42:01 +0200

    terminal

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+};
